a75bb212bee1daae208867c97841ced860d8b838
SMauhourat <santiagomauhourat@hotmail.com>
2023-03-19 19:19:16 -0300

    call api

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,7 +2,9 @@ const request = require('postman-request');
 
 const url = "http://api.weatherstack.com/current?access_key=6962c0c3606eb129fae12c0b74417542&query=Buenos%20Aires"
 
-request({ url: url }, (error, response, body) => {
-    const data = JSON.parse(body);
-    console.log(data)
+request({ url: url, json: true }, (error, response, body) => {
+    console.log(body.current)
+    //temperature
+    //feelslike
+    console.log('It is currenttly', body.current.temperature, 'degrees out. It feels like', body.current.feelslike, 'degrees out.')
 })
