d0b374a90b92cf0365815e1cbad43c47841f916b
SMauhourat <santiagomauhourat@hotmail.com>
2023-03-20 09:47:53 -0300

    call with args

--- weather-app/app.js
+++ weather-app/app.js
@@ -29,15 +29,21 @@ const forecast = require('./utils/forecast')
 // })  
 
 
-geocode('Buenos Aires', (error, data) => {
-    if (error) {
-        return console.log(error)
-    }
+const address = process.argv[2]
 
-    forecast(data.latitude,data.longitude, (error, data) => {
-        if (error)
+if (!address) {
+    return console.log('Please provide a valid address')
+} else {
+    geocode(process.argv[2], (error, data) => {
+        if (error) {
             return console.log(error)
-        
-        console.log(data)
-    })          
-})
+        }
+    
+        forecast(data.latitude,data.longitude, (error, data) => {
+            if (error)
+                return console.log(error)
+            
+            console.log(data)
+        })          
+    })
+}
