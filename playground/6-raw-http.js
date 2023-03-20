188db302b8e22f9f37a4523676fd29a2644b2360
Santiago Mauhourat <smauhourat@aadim.org.ar>
2023-03-20 17:48:31 -0300

    raw http

---
+++ playground/6-raw-http.js
@@ -0,0 +1,8 @@
+const https = require('http')
+
+const url = "http://api.weatherstack.com/forecast?access_key=6962c0c3606eb129fae12c0b74417542&query=40,-75"
+
+
+http.request(url, (response) => {
+
+})
