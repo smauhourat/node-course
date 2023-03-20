8c2a3b4dab6f7713ccfcff07b3919b6c36dd70d7
SMauhourat <santiagomauhourat@hotmail.com>
2023-03-20 08:18:28 -0300

    callback example

---
+++ playground/4-callbacks.js
@@ -0,0 +1,22 @@
+// setTimeout(() => {
+//     console.log('Two seconds are up')
+// }, 2000)
+
+//
+// Goal: Mess around with the callback pattern
+//
+// 1. Define an add function that accepts the correct arguments
+// 2. Use setTimeout to simulate a 2 second delay
+// 3. After 2 seconds are up, call the callback function with the sum
+// 4. Test your work!
+
+const add = (a, b, cb) => {
+    setTimeout(() => {
+        const data = a + b
+        cb (data)
+    }, 2000)
+}
+
+add(1, 4, (sum) => {
+    console.log(sum) // Should print: 5
+})
