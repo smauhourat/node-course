3d53cdb513ad69abb292b2b497b92bb7172baa75
SMauhourat <santiagomauhourat@hotmail.com>
2023-03-20 08:35:57 -0300

    callback geocode

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,4 +1,5 @@
-const request = require('postman-request');
+const request = require('postman-request')
+const geocode = require('./utils/geocode')
 
 //const url = "http://api.weatherstack.com/current?access_key=6962c0c3606eb129fae12c0b74417542&query=Buenos%20Aires"
 //const url = "http://api.weatherstack.com/current?access_key=6962c0c3606eb129fae12c0b74417542&query=-34.607357,-58.454595"
@@ -20,21 +21,33 @@ const request = require('postman-request');
 
 //const geocodeUrl = "https://api.radar.io/v1/geocode/forward?query=841+broadway+new+york"
 
-const geocodeUrl = "https://api.radar.io/v1/geocode/forward?query=EstoEsUnerror"
-const auth_api = "prj_test_pk_7dc5278af7990f2c961f721ad0b8d8078a854f78"
+// const geocodeUrl = "https://api.radar.io/v1/geocode/forward?query=EstoEsUnerror"
+// const auth_api = "prj_test_pk_7dc5278af7990f2c961f721ad0b8d8078a854f78"
 
-request({ url: geocodeUrl, json: true, headers: { 'Authorization' : auth_api} }, (error, response, body) => {
+// request({ url: geocodeUrl, json: true, headers: { 'Authorization' : auth_api} }, (error, response, body) => {
 
-    // console.log(body)
-    // console.log(error)
+//     // console.log(body)
+//     // console.log(error)
 
-    if (error) {
-        console.log(error)
-    } else if (body.addresses.length == 0) {
-        console.log('Service currently not available')
-    } else {
-        const latitude = body.addresses[0].latitude
-        const longitude = body.addresses[0].longitude
-        console.log(latitude, longitude)
-    }
+//     if (error) {
+//         console.log(error)
+//     } else if (body.addresses.length == 0) {
+//         console.log('Service currently not available')
+//     } else {
+//         const latitude = body.addresses[0].latitude
+//         const longitude = body.addresses[0].longitude
+//         console.log(latitude, longitude)
+//     }
+// })
+
+
+geocode('Buenos Aires', (error, data) => {
+    console.log('Error:', error)
+    console.log('Data:', data)
+})
+
+
+geocode('8a76sda6s8a7sd867a', (error, data) => {
+    console.log('Error:', error)
+    console.log('Data:', data)
 })
---
+++ weather-app/utils/geocode.js
@@ -0,0 +1,25 @@
+const request = require('postman-request')
+
+const geocode = (address, callback) => {
+    const url = "https://api.radar.io/v1/geocode/forward?query=" + address
+    const auth_api = "prj_test_pk_7dc5278af7990f2c961f721ad0b8d8078a854f78"
+
+    request({ url: url, json: true, headers: { 'Authorization' : auth_api} }, (error, response, body) => {
+        if (error) {
+            callback('Network error', undefined)
+        } else if (body.addresses.length == 0) {
+            callback('Service currently not available', undefined)
+        } else {
+            const latitude = body.addresses[0].latitude
+            const longitude = body.addresses[0].longitude
+            const location = body.addresses[0].formattedAddress
+            callback(undefined, {
+                latitude: latitude,
+                longitude: longitude,
+                location: location
+            })
+        }
+    })
+}
+
+module.exports = geocode
