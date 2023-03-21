6d32260af3e4ca93765f90637b013d0cc3bf54b1
Santiago Mauhourat <smauhourat@aadim.org.ar>
2023-03-21 13:01:48 -0300

    http node

--- playground/6-raw-http.js
+++ playground/6-raw-http.js
@@ -1,8 +1,23 @@
-const https = require('http')
+const http = require('http')
 
 const url = "http://api.weatherstack.com/forecast?access_key=6962c0c3606eb129fae12c0b74417542&query=40,-75"
 
 
-http.request(url, (response) => {
+const request = http.request(url, (response) => {
+    let data = ''
+
+    response.on('data', (chunk) => {
+        data = data + chunk.toString()
+    })
+
+    response.on('end', () => {
+        console.log(JSON.parse(data))
+    })
 
 })
+
+request.on('error', (error) => {
+    console.log('An ERROR', error)
+})
+
+request.end()
