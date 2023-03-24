22073a0502db9ad1d1af71108b7bdc6e0d3817d6
SMauhourat <santiagomauhourat@hotmail.com>
2023-03-24 14:33:39 -0300

    static page

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,7 +2,32 @@ const express = require('express')
 
 const app = express()
 
+console.log(__dirname)
+console.log(__filename)
+
+const path = require('path')
+app.use(express.static(path.join(__dirname, '../public')))
+
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
     res.send('hello world')
+})
+
+// app.get('/help', (req, res) => {
+//     res.send('Help')
+// })
+
+// app.get('/about', (req, res) => {
+//     res.send('<h1>About</h1>')
+// })
+
+app.get('/weather', (req, res) => {
+    res.send({
+        location: 'Buenos Aires',
+        forecast: 'Soleado, 32 grados'
+    })
+})
+
+app.listen(3000,() => {
+    console.log('Server running in port 3000')
 })
